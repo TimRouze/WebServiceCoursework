f26c06c64af115c07ec5334bc145899e61a6c0d4
unknown <timothe.rouze1@gmail.com>
2020-05-02 18:51:10 +0200

    about content

--- view/about.php
+++ view/about.php
@@ -17,3 +17,12 @@
     </ul>
   </div>
 </nav>
+
+<div class="row">
+    <div class="col s6">
+        <h3>Goal of this website:</h3>
+        <p>The purpose of this websystem is to give to user a readable list of selected RSS feeds.
+        Administrators can add or delete websites from the list to restrain or improve the feed selection.
+        They can also change the number of news displayed per page and they can also refresh the feed manually.</p>
+    </div>
+</div>
