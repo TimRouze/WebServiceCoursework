0af32457eea72fb6c7bacaca1dad46bd43a95639
unknown <timothe.rouze1@gmail.com>
2020-05-02 17:27:32 +0200

    db

--- sql/initDB.sql
+++ sql/initDB.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 02, 2020 at 03:17 PM
+-- Server version: 8.0.19-0ubuntu5
+-- PHP Version: 7.4.3
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -9,46 +18,97 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `nebulae`
+--
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
 DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
+CREATE TABLE `admin` (
   `login` varchar(15) NOT NULL,
-  `password` varchar(150) NOT NULL,
-  PRIMARY KEY (`login`),
-  UNIQUE KEY `login` (`login`)
+  `password` varchar(150) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-
 INSERT INTO `admin` (`login`, `password`) VALUES
-('admin', '$2y$10$kVdUzgj.H7R6Ut9u88Qd0uCjLPsWzOvnVQKzJCoVC69Jc4FCSdLMCf');
+('admin', '$2y$10$df7pq0DYr.YiEgXfx1upqurb/EZSfjXFo9wu3pC0glAyAmBL2Kb92');
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `news`
+--
 DROP TABLE IF EXISTS `news`;
-CREATE TABLE IF NOT EXISTS `news` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_Site` int(11) NOT NULL,
+CREATE TABLE `news` (
+  `id` int NOT NULL,
+  `id_Site` int NOT NULL,
   `date` datetime NOT NULL,
-  `SiteName` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Title` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `SiteName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Title` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `Description` blob NOT NULL,
   `Url` blob NOT NULL,
-  `UrlWebsite` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_Site` (`id_Site`)
+  `UrlWebsite` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
+--
+-- Table structure for table `sites`
+--
 DROP TABLE IF EXISTS `sites`;
-CREATE TABLE IF NOT EXISTS `sites` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `sites` (
+  `ID` int NOT NULL,
   `name` varchar(100) NOT NULL,
-  `fluxURL` varchar(100) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `fluxURL` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `sites`
+--
 
 INSERT INTO `sites` (`ID`, `name`, `fluxURL`) VALUES
-(2, 'JeuxVideo.com', 'http://www.jeuxvideo.com/rss/rss.xml');
+(3, 'BBC World', 'http://feeds.bbci.co.uk/news/world/rss.xml');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`login`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_Site` (`id_Site`);
+
+--
+-- Indexes for table `sites`
+--
+ALTER TABLE `sites`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `sites`
+--
+ALTER TABLE `sites`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
