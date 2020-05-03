e706c3f34dd79522639f4a23a0fe36ff8d09ff21
unknown <timothe.rouze1@gmail.com>
2020-05-03 11:49:55 +0200

    Tests grid

--- view/about.php
+++ view/about.php
@@ -18,35 +18,47 @@
   </div>
 </nav>
 
-<div class="row center-block">
-    <div class="col s7">
+<div class="row">
+    <div class="col s12 m4 l12"></div>
+    <div class="col s12 m4 l8">
         <h3>Goal of this website:</h3>
         <p>The purpose of this websystem is to give to user a readable list of selected RSS feeds.
         Administrators can add or delete websites from the list to restrain or improve the feed selection.
         They can also change the number of news displayed per page and they can also refresh the feed manually.</p>
     </div>
+    <div class="col s12 m4 l2"></div>
 </div>
 
-<div class="row center-block">
-    <div class="col s7 offset-s3">
-        <div class="col s4"><image src="view/Images/materialize_logo.png" alt="Phpmyadmin logo"></image></div>
-        <div class="col s6"><h3>MaterializeCss</h3></div>
+<div class="row">
+    <div class="col s12 m4 l2"><p>s12 m4</p></div>
+    <div class="col s12 m4 l8"><p>s12 m4</p></div>
+    <div class="col s12 m4 l2"><p>s12 m4</p></div>
+  </div>
+
+<div class="row">
+    <div class="col s12 m4 l2"></div>
+    <div class="col s12 m4 l8">
+        <image src="view/Images/materialize_logo.png" alt="Phpmyadmin logo"></image>
+        <h3>MaterializeCss</h3>
         <p><a href="https://materializecss.com/">Materialize CSS</a> is a library that allows for visually appealing designs for websites.
         It is easy to use and follows standards to give consistent design to web applications.</p>
     </div>
+    <div class="col s12 m4 l2"></div>
 </div>
 
-<div class="row center-block">
-    <div class="col s7">
-        <div class="col s4"><image src="view/Images/AWS_logo.png" alt="Phpmyadmin logo"></image></div>
-        <div class="col s6"><h3>AWS EC2</h3></div>
+<div class="row">
+    <div class="col s12 m4 l2"></div>
+    <div class="col s12 m4 l8">
+        <image src="view/Images/AWS_logo.png" alt="Phpmyadmin logo"></image>
+        <h3>AWS EC2</h3>
         <p><a href="https://aws.amazon.com/">Amazon Web Service</a> is a powerful platform specialised in on demand cloud computing services. AWS offers APIs, Databases, Virtual machines, etc... 
         Amazon web services where used on the project for the hosting part of the website to make it accessible from everywhere. </p>
     </div>
+    <div class="col s12 m4 l2"></div>
 </div>
 
 <div class="row center-block">
-    <div class="col s7">
+    <div class="col s7 offset-s4">
         <div class="col s4"><image src="view/Images/phpmyadmin_logo.png" alt="Phpmyadmin logo"></image></div>
         <div class="col s6"><h3>Phpmyadmin</h3></div>
         <p><a href="https://www.phpmyadmin.net/">Phpmyadmin</a> is a free software tool that allows to handle the administration of MySQL over the Web.
