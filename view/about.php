be0af1d33037c5184256d30cdf33af9b46fdfa80
unknown <timothe.rouze1@gmail.com>
2020-05-03 12:59:37 +0200

    text size

--- view/about.php
+++ view/about.php
@@ -27,8 +27,8 @@
 </div>
 <div class="row">
     <div class="col s12 m4 l2"></div>
-    <div class="col s12 m4 l8">
-        <p style="text-align:center">The purpose of this websystem is to give to user a readable list of selected RSS feeds.
+    <div class="col s12 m4 l8" style="text-align:center">
+        <p style="font-size:25px">The purpose of this websystem is to give to user a readable list of selected RSS feeds.
         Administrators can add or delete websites from the list to restrain or improve the feed selection.
         They can also change the number of news displayed per page and they can also refresh the feed manually.</p>
     </div>
@@ -38,7 +38,7 @@
 <div class="row">
     <div class="col s12 m4 l4"></div>
     <div class="col s12 m4 l4" style="text-align:center">
-        <img  src="view/Images/materialize_logo.png" alt="Phpmyadmin logo"></img>
+        <img  src="view/Images/materialize_logo.png" alt="Materialize css logo"/>
         <h3>MaterializeCss</h3>
         
     </div>
@@ -46,8 +46,8 @@
 </div>
 <div class="row">
     <div class="col s12 m4 l2"></div>
-    <div class="col s12 m4 l8">
-        <p style="text-align:center"><a href="https://materializecss.com/">Materialize CSS</a> is a library that allows for visually appealing designs for websites.
+    <div class="col s12 m4 l8" style="text-align:center">
+        <p style="font-size:25px"><a href="https://materializecss.com/">Materialize CSS</a> is a library that allows for visually appealing designs for websites.
         It is easy to use and follows standards to give consistent design to web applications.</p>
         </div>
     <div class="col s12 m4 l2"></div>
@@ -56,15 +56,15 @@
 <div class="row">
     <div class="col s12 m4 l4"></div>
     <div class="col s12 m4 l4" style="text-align:center">
-        <img src="view/Images/AWS_logo.png" alt="Amazon web service logo"></img>
+        <img src="view/Images/AWS_logo.png" alt="Amazon web service logo"/>
         <h3>AWS EC2</h3>
     </div>
     <div class="col s12 m4 l4"></div>
 </div>
 <div class="row">
     <div class="col s12 m4 l2"></div>
-    <div class="col s12 m4 l8">
-        <p style="text-align:center"><a href="https://aws.amazon.com/">Amazon Web Service</a> is a powerful platform specialised in on demand cloud computing services. AWS offers APIs, Databases, Virtual machines, etc... 
+    <div class="col s12 m4 l8" style="text-align:center">
+        <p style="font-size:25px"><a href="https://aws.amazon.com/">Amazon Web Service</a> is a powerful platform specialised in on demand cloud computing services. AWS offers APIs, Databases, Virtual machines, etc... 
         Amazon web services where used on the project for the hosting part of the website to make it accessible from everywhere. </p>
     </div>
     <div class="col s12 m4 l2"></div>
@@ -73,15 +73,15 @@
 <div class="row">
     <div class="col s12 m4 l4"></div>
     <div class="col s12 m4 l4" style="text-align:center">
-        <img src="view/Images/mySql_logo.png" alt="MySQL logo"></img>
+        <img src="view/Images/mySql_logo.png" alt="MySQL logo"/>
         <h3>MySQL</h3>
     </div>
     <div class="col s12 m4 l4"></div>
 </div>
 <div class="row">
     <div class="col s12 m4 l2"></div>
-    <div class="col s12 m4 l8">
-        <p style="text-align:center"><a href="https://www.mysql.com/en/">MySql</a> is a database management system. It uses SQL as primary language and
+    <div class="col s12 m4 l8" style="text-align:center">
+        <p style="font-size:25px"><a href="https://www.mysql.com/en/">MySql</a> is a database management system. It uses SQL as primary language and
         is open source.</p>
     </div>
     <div class="col s12 m4 l2"></div>
@@ -90,15 +90,15 @@
 <div class="row">
     <div class="col s12 m4 l4"></div>
     <div class="col s12 m4 l4" style="text-align:center">
-        <img src="view/Images/phpmyadmin_logo.png" alt="Phpmyadmin logo"></img>
+        <img src="view/Images/phpmyadmin_logo.png" alt="Phpmyadmin logo"/>
         <h3>Phpmyadmin</h3>
     </div>
     <div class="col s12 m4 l4"></div>
 </div>
 <div class="row">
     <div class="col s12 m4 l2"></div>
-    <div class="col s12 m4 l8">
-        <p style="text-align:center"><a href="https://www.phpmyadmin.net/">Phpmyadmin</a> is a free software tool that allows to handle the administration of MySQL over the Web.
+    <div class="col s12 m4 l8" style="text-align:center">
+        <p style="font-size:25px"><a href="https://www.phpmyadmin.net/">Phpmyadmin</a> is a free software tool that allows to handle the administration of MySQL over the Web.
         phpMyAdmin supports a wide range of operations on MySQL and MariaDB and offers a practical user interface to perform operations on a DB.</p>
     </div>
     <div class="col s12 m4 l2"></div>
