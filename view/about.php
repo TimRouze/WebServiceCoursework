d7704c7b5965c51a491c23f8b542d83a2ffeb28d
unknown <timothe.rouze1@gmail.com>
2020-05-03 17:08:34 +0200

    Licenses

--- view/about.php
+++ view/about.php
@@ -49,6 +49,7 @@
     <div class="col s12 m4 l8" style="text-align:center">
         <p style="font-size:17px"><a href="https://materializecss.com/">Materialize CSS</a> is a library that allows for visually appealing designs for websites.
         It is easy to use and follows standards to give consistent design to web applications.</p>
+        <p style="font-size:17px">Code Copyright 2018 Materialize. Code released under the MIT license.</p>
         </div>
     <div class="col s12 m4 l2"></div>
 </div>
@@ -67,6 +68,7 @@
     <div class="col s12 m4 l8" style="text-align:center">
         <p style="font-size:17px"><a href="https://aws.amazon.com/">Amazon Web Service</a> is a powerful platform specialised in on demand cloud computing services. AWS offers APIs, Databases, Virtual machines, etc... 
         Amazon web services where used on the project for the hosting part of the website to make it accessible from everywhere. </p>
+        <p style="font-size:17px"> Amazon web services offer free on demand virtual machines for a usage of 750Hrs/ months and for a duration of 12 months. They apply on the go charging after that.</p>
     </div>
     <div class="col s12 m4 l2"></div>
 </div>
@@ -85,6 +87,7 @@
     <div class="col s12 m4 l8" style="text-align:center">
         <p style="font-size:17px"><a href="https://www.mysql.com/en/">MySql</a> is a database management system. It uses SQL as primary language and
         is open source.</p>
+        <p style="font-size:17px">MySQL is released under GNU General Public License, version 2 as long as the use is for Open source projects.</p>
     </div>
     <div class="col s12 m4 l2"></div>
 </div>
@@ -103,6 +106,9 @@
         <div class="col s12 m4 l8" style="text-align:center">
             <p style="font-size:17px"><a href="https://www.phpmyadmin.net/">Phpmyadmin</a> is a free software tool that allows to handle the administration of MySQL over the Web.
             phpMyAdmin supports a wide range of operations on MySQL and MariaDB and offers a practical user interface to perform operations on a DB.</p>
+            <p style="font-size:17px"> phpMyAdmin is released under GNU General Public License, version 2.
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 2 of the License. </p>
         </div>
         <div class="col s12 m4 l2"></div>
     </div>
